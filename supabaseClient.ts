b5d9e7c5c05fcaafc20d7b5c801f24915a18a27e
furuhana <140679951+furuhana@users.noreply.github.com>
2025-12-16 23:26:12 +0800

    修改了部分参数

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // 🔴 请去 Supabase 后台 -> Project Settings -> API 复制这两个值
 // 为了方便调试，你可以先直接填在这里。正式上线 Vercel 时再改成环境变量。
-const supabaseUrl = '你的_Supabase_Project_URL'
-const supabaseKey = '你的_Supabase_Anon_Key'
+const supabaseUrl = 'https://nxkamhepawefvcvtmfrl.supabase.co'
+const supabaseKey = 'sb_publishable_3wXbMevhtHu3j4NOIsqxEA_Wb0q1Kmm'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
